d82d24e320bea1592c9d329797eafdec5ff0fdb6
Vita <vitalia@myrambler.ru>
2020-02-08 11:05:53 +0300

    + new tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,33 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var del = require("del");
 
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+gulp.task("copy", function() {
+  return gulp
+    .src(["source/fonts/**/*.{woff,woff2}", "source/img/**", "source/js/**"], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("css", function() {
+  return gulp
+    .src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
 
-gulp.task("server", function () {
+gulp.task("server", function() {
   server.init({
     server: "source/",
     notify: false,
